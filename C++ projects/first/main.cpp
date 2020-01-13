d5098423110c0dd783f562b30885397fd70665e6
Ryan Tran <Phuong16032001@gmail.com>
2020-01-13 23:12:51 +0700

    Bought the stuff I listed

--- C++ projects/first/main.cpp
+++ C++ projects/first/main.cpp
@@ -14,8 +14,11 @@ int main()
     cin >> (Age);
     cout << "You are " << Age << " years old,is that correct?" endl;
     cout << "Let's talk about my new laptop" << endl;
+    cout << "It's the 2019 base-model 13 inch MacBook Pro,it has a quad core i5 processor,8gb of ram and 128gb SSD." << endl;
+    cout << "Probably a downgrade,specs wise.But the OS(Operating System) and the design of this laptop make up for itself." << endl;
+    cout << "Not to mention,the awesome trackpad,the retina display,the keyboard feels nice to type on but it will have some problem,I mean,my keyboard has a broken return key" << endl;
+    cout << "To be honest I would love to spend $1000 for this,not $1300,because I would use the $300 for a mechanical keyboard and external SSD drive(s)" << endl;
+    
     return 0;
 }
-// Hello everyone,I am Ryan and my real Vietnamese name is Phuong
-// My laptop is a Dell Latitude 7480,should I have bought a MacBook?
-// I have bought a new laptop and it's the 2019 13-inch base model MacBook Pro
+
--- Python projects/Hello World.py
+++ Python projects/Hello World.py
@@ -1,2 +1,8 @@
 print("Hello World!")
 # Welcome to Python 3
+name = (input("Please insert your name:"))
+print("Hi " + name + ".This is Ryan's MacBook Pro.His first one that have the keyboard issue and 8gb of ram is not enough.")
+print("This is the first time I code using Python on VSC editor")
+print("This is the first time I use my MacBook pro with a mechanical keyboard.And the keyboard was the Ducky One 2 SF RGB.")
+print("I chose the blue switch just because of personal refrences and I don't give a flying fuck about what people think or if they are being annoyed by my fucking loud keyboard!")
+print("Also I have bought the AirPods Pro and a bunch of stuff that cost quite a lot")
